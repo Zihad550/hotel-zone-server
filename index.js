0bc704f3df6629870129d8a06748279e74b2ff34
Zihad550 <hussainmuzahid83@gmail.com>
2021-12-21 14:11:48 +0600

    booking delete route added

--- index.js
+++ index.js
@@ -24,29 +24,6 @@ async function run() {
     const hotelBookedCollection = database.collection("hotelBooked");
     const citiesCollection = database.collection("cities");
 
-    // get all the class
-    app.get("/hotels", async (req, res) => {
-      const cursor = hotelsCollections.find({});
-      const hotels = await cursor.toArray();
-      res.send(hotels);
-    });
-
-    // use post to get data by keys
-    app.get("/hotels/:city", async (req, res) => {
-      const cityName = req.params.city;
-      const query = { city: cityName };
-      const cursor = hotelsCollections.find(query);
-      const result = await cursor.toArray();
-      res.json(result);
-    });
-    // get a single class
-    app.get("/hotels/hotel/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const hotel = await hotelsCollections.findOne(query);
-      res.json(hotel);
-    });
-
     // set reviews
     app.post("/reviews", async (req, res) => {
       const review = req.body;
@@ -67,10 +44,26 @@ async function run() {
       res.json(result);
     });
 
+    // delete reviews
+    app.delete("/reviews", async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.json(result);
+    });
+
     // post booked hotel
     app.post("/booked", async (req, res) => {
       const hotel = req.body;
       const result = await hotelBookedCollection.insertOne(hotel);
+      res.json(result);
+    });
+
+    //delete bookings
+    app.delete("/booked", async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: Object(id) };
+      const result = await hotelBookedCollection.deleteOne(query);
       res.json(result);
     });
 
